4225694836d37654f0dbedebdf8b45523aa9a96e
MIsha-Korneew <korneewmisha2006@gmail.com>
2025-05-27 19:57:04 +0300

    Lesson A Lesson B Lesson C

--- src/by/it/group410901/korneew/lesson06/C_LongNotUpSubSeq.java
+++ src/by/it/group410901/korneew/lesson06/C_LongNotUpSubSeq.java
@@ -2,68 +2,82 @@ package by.it.group410901.korneew.lesson06;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
+/*
+Задача на программирование: наибольшая невозростающая подпоследовательность
+
+Дано:
+    целое число 1<=n<=1E5 ( ОБРАТИТЕ ВНИМАНИЕ НА РАЗМЕРНОСТЬ! )
+    массив A[1…n] натуральных чисел, не превосходящих 2E9.
+
+Необходимо:
+    Выведите максимальное 1<=k<=n, для которого гарантированно найдётся
+    подпоследовательность индексов i[1]<i[2]<…<i[k] <= длины k,
+    для которой каждый элемент A[i[k]] не больше любого предыдущего
+    т.е. для всех 1<=j<k, A[i[j]]>=A[i[j+1]].
+
+    В первой строке выведите её длину k,
+    во второй - её индексы i[1]<i[2]<…<i[k]
+    соблюдая A[i[1]]>=A[i[2]]>= ... >=A[i[n]].
+
+    (индекс начинается с 1)
+
+Решить задачу МЕТОДАМИ ДИНАМИЧЕСКОГО ПРОГРАММИРОВАНИЯ
+
+    Sample Input:
+    5
+    5 3 4 4 2
+
+    Sample Output:
+    4
+    1 3 4 5
+*/
 
 
 public class C_LongNotUpSubSeq {
 
-    public static void main(String[] args) throws FileNotFoundException {
-        InputStream stream = C_LongNotUpSubSeq.class.getResourceAsStream("dataC.txt");
-        C_LongNotUpSubSeq instance = new C_LongNotUpSubSeq();
-        instance.findLongestNonIncreasingSubsequence(stream);
-    }
+    int getLongest(int[] arr) {
 
-    void findLongestNonIncreasingSubsequence(InputStream stream) throws FileNotFoundException {
-        Scanner scanner = new Scanner(stream);
-        int n = scanner.nextInt(); // Читаем длину последовательности
-        int[] numbers = new int[n];
-
-        // Читаем саму последовательность
+        int n = arr.length;
+        int[] array = new int[n];
+        int maxLen = 1;
         for (int i = 0; i < n; i++) {
-            numbers[i] = scanner.nextInt();
-        }
-
-        int[] dp = new int[n]; // Массив для хранения длины подпоследовательности
-        int[] previousIndex = new int[n]; // Массив для хранения предыдущих индексов
-        int maxLength = 0; // Максимальная длина невозрастающей подпоследовательности
-        int lastIndex = -1; // Индекс последнего элемента в последовательности
-
-        // Инициализация массивов
-        for (int i = 0; i < n; i++) {
-            dp[i] = 1; // Каждое число само по себе является подпоследовательностью
-            previousIndex[i] = -1; // Изначально нет предшественников
-        }
-
-        // Динамическое программирование для нахождения длины подпоследовательности
-        for (int i = 1; i < n; i++) {
+            array[i] = 1;
             for (int j = 0; j < i; j++) {
-                if (numbers[i] <= numbers[j] && dp[i] < dp[j] + 1) {
-                    dp[i] = dp[j] + 1; // Обновляем длину подпоследовательности
-                    previousIndex[i] = j; // Запоминаем индекс предшествующего элемента
+                if (arr[i] <= arr[j]) {
+                    array[i] = Math.max(array[i], array[j] + 1);
                 }
             }
-            // Обновляем максимальную длину и индекс последнего элемента
-            if (dp[i] > maxLength) {
-                maxLength = dp[i];
-                lastIndex = i;
-            }
+            maxLen = Math.max(maxLen, array[i]);
         }
-
-        // Восстанавливаем индексы наибольшей невозрастающей подпоследовательности
-        List<Integer> indices = new ArrayList<>();
-        while (lastIndex != -1) {
-            indices.add(lastIndex + 1); // Индексы начинаются с 1
-            lastIndex = previousIndex[lastIndex];
-        }
-
-        // Печатаем результат
-        System.out.println(maxLength); // Длина подпоследовательности
-        for (int i = indices.size() - 1; i >= 0; i--) {
-            System.out.print(indices.get(i) + " "); // Индексы в порядке возрастания
-        }
-        System.out.println(); // Переход на новую строку
+        return maxLen;
     }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        InputStream stream = B_LongDivComSubSeq.class.getResourceAsStream("dataC.txt");
+        C_LongNotUpSubSeq instance = new C_LongNotUpSubSeq();
+        int result = instance.getNotUpSeqSize(stream);
+        System.out.print(result);
+    }
+
+    int getNotUpSeqSize(InputStream stream) throws FileNotFoundException {
+        //подготовка к чтению данных
+        Scanner scanner = new Scanner(stream);
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        //общая длина последовательности
+        int n = scanner.nextInt();
+        int[] m = new int[n];
+        //читаем всю последовательность
+        for (int i = 0; i < n; i++) {
+            m[i] = scanner.nextInt();
+        }
+        //тут реализуйте логику задачи методами динамического программирования (!!!)
+        int result = getLongest(m);
+
+
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        return result;
+    }
+
 }
