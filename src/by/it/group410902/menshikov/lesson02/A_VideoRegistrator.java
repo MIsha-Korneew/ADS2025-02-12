8e69ce173da0b696f065bed558e643990a77e3a2
MIsha-Korneew <korneewmisha2006@gmail.com>
2025-12-10 11:15:13 +0300

    Fixes

--- src/by/it/group410902/menshikov/lesson02/A_VideoRegistrator.java
+++ src/by/it/group410902/menshikov/lesson02/A_VideoRegistrator.java
@@ -1,7 +1,6 @@
-package by.it.group451003.rabbimov.lesson02;
+package by.it.group410902.menshikov.lesson02;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 /*
 Даны события events
@@ -24,28 +23,47 @@ public class A_VideoRegistrator {
     List<Double> calcStartTimes(double[] events, double workDuration) {
         //events - события которые нужно зарегистрировать
         //timeWorkDuration время работы видеокамеры после старта
+        insertionSort(events);
         List<Double> result;
         result = new ArrayList<>();
-        int i = 0;                              //i - это индекс события events[i]
+        int i = 0;
+        //i - это индекс события events[i]
         //Комментарии от проверочного решения сохранены для подсказки, но вы можете их удалить.
         //Подготовка к жадному поглощению массива событий
         //hint: сортировка Arrays.sort обеспечит скорость алгоритма
         //C*(n log n) + C1*n = O(n log n)
 
-        Arrays.sort(events);
-        result.add(events[0]);
-        for (i = 1; i < events.length; i++)
-            if (events[i] > result.get(result.size() - 1) + 1)
-                result.add(events[i]);
         //пока есть незарегистрированные события
         //получим одно событие по левому краю
         //и запомним время старта видеокамеры
         //вычислим момент окончания работы видеокамеры
         //и теперь пропустим все покрываемые события
         //за время до конца работы, увеличивая индекс
+        int n = events.length;
 
+        while (i < n) {
+            double start = events[i]; // Запоминаем время старта видеорегистратора
+            result.add(start);
+            double end = start + workDuration; // Вычисляем момент окончания работы видеорегистратора
+
+            while (i < n && events[i] <= end) {
+                i++;
+            } // Пропускаем все события, которые происходят в пределах времени работы видеорегистратора
+        }
 
         return result;                        //вернем итог
     }
-}
+    private void insertionSort(double[] array) {
+        for (int i = 1; i < array.length; i++) {
+            double key = array[i];
+            int j = i - 1;
 
+            // Перемещаем элементы, которые больше ключа, на одну позицию вперед
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+        }
+    }
+}
